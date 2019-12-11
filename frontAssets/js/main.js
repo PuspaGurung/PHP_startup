2bac3d664d677054f24ae2c4d27d29648f9171bf
Puspa <gpuspamail@gmail.com>
2019-12-11 13:16:25 +0100

    create table

--- frontAssets/js/main.js
+++ frontAssets/js/main.js
@@ -2,34 +2,30 @@ window.onload = handleGetArticles();
 
 //Get All Posts
 function handleGetArticles() {
-  fetch('http://localhost/PHP_liveArticle/backAssets/api/getArticles.php')
-    .then((res) => res.json())
-    .then((articles) => {
-      console.log(articles)
-      // let article = "";
+  fetch("http://localhost/PHP_liveArticle/backAssets/api/getArticles.php")
+    .then(res => res.json())
+    .then(articles => {
       articles.article.map(function (article) {
-        console.log(article)
+        console.log(article);
       });
     })
-    .catch((err) => console.log(err))
+    .catch(err => console.log(err));
 }
-
 
 //Create Article
 function handleCreateArticle(formData) {
   let data = new FormData(formData);
-  console.log(formData.action);
   fetch(formData.action, {
       method: formData.method,
       body: data
     })
-    .then((res) => res.json())
-    .then((data) => {
-      document.getElementById('article-table').innerHTML = data.message;
-      //Empty the form after submit 
-      document.getElementById('submit-article').reset();
+    .then(res => res.json())
+    .then(data => {
+      document.getElementById("article-table").innerHTML = data.message;
+      //Empty the form after submit
+      document.getElementById("submit-article").reset();
       handleGetArticles(); // calling get posts function
     })
-    .catch((err) => console.log(err))
+    .catch(err => console.log(err));
   return false; //prevent form from posting (element.preventDefault())
 }
