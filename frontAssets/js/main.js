1d9c24d03ed9549eb2f43ff12fe5eb66b3b76238
Puspa <gpuspamail@gmail.com>
2019-12-15 02:10:30 +0100

    Handle pagination

--- frontAssets/js/main.js
+++ frontAssets/js/main.js
@@ -1,57 +1,42 @@
 window.onload = handleGetArticles();
 
 // Get DOM element
-let domControl = {
-  // Table
+let DOMcontrol = {
   articleWrapper: document.querySelector(".articles-wrapper"),
+  // Table 
   tableArticle: document.querySelector(".articles-wrapper"),
+  // Table head
   tableArticleHeader: document.querySelector(".table-article__head"),
-  tableArticleBody: document.querySelector(".table-article__body"),
-  linkCreateArticleForm: document.querySelector(".link-create-article-form"),
   tableHeaderRow: document.querySelector(".thead-row"),
   tableHeaderTh: document.querySelector(".thead-row__th"),
+  linkCreateArticleForm: document.querySelector(".link-create-article-form"),
+  // Table body
+  tableArticleBody: document.querySelector(".table-article__body"),
+  // Table foot
+  btnLoadMore: document.querySelector(".more-article"),
+  totalArticleNumber: document.querySelector('.total-article-number'),
+  totalLoadedArticleNumber: document.querySelector('.loaded-article-number'),
 
   // Sort element
   sort: document.querySelector(".sort"),
   sortAsc: document.querySelector(".sort__asc"),
   sortDesc: document.querySelector(".sort__desc"),
-  sortAscTitle: document.querySelector(".sort__asc-title"),
-  sortDescTitle: document.querySelector(".sort__desc-title"),
-  sortAscPublished: document.querySelector(".sort__asc-published"),
-  sortDescPublished: document.querySelector(".sort__desc-published"),
-  sortAscSite: document.querySelector(".sort__asc-site"),
-  sortDescSite: document.querySelector(".sort__desc-site"),
-  sortAscAd: document.querySelector(".sort__asc-ad"),
-  sortDescAd: document.querySelector(".sort__desc-ad"),
-  sortAscBids: document.querySelector(".sort__asc-bids"),
-  sortDescBids: document.querySelector(".sort__desc-bids"),
-  sortAscSpending: document.querySelector(".sort__asc-spending"),
-  sortDescSpending: document.querySelector(".sort__desc-spending"),
-  sortAscWin: document.querySelector(".sort__asc-Win"),
-  sortDescWin: document.querySelector(".sort__desc-Win"),
-  sortAscImpressions: document.querySelector(".sort__asc-Impressions"),
-  sortDescImpressions: document.querySelector(".sort__desc-Impressions"),
-  sortAscClicks: document.querySelector(".sort__asc-Clicks"),
-  sortDescClicks: document.querySelector(".sort__desc-Clicks"),
-  sortAscCtr: document.querySelector(".sort__asc-ctr"),
-  sortDescCtr: document.querySelector(".sort__desc-ctr"),
 
   // Link to submit form
-  linkCreateArticle: document.querySelector('.link-create-article '),
+  linkCreateArticle: document.querySelector(".link-create-article "),
   // Popup
-  popupOverlay: document.querySelector('#popup-overlay'),
-  closePopupOverlay: document.querySelector('.popup-close'),
+  popupOverlay: document.querySelector("#popup-overlay"),
+  closePopupOverlay: document.querySelector(".popup-close"),
   //Submit form
-  submitArticleForm: document.querySelector('#submit-article'),
+  submitArticleForm: document.querySelector("#submit-article"),
   // Submit message
-  submitMessage: document.querySelector('.submit-message-box'),
+  submitMessage: document.querySelector(".submit-message-box"),
   // Cancel submit message
-  clearSubmitForm: document.querySelector('.form-group__btn-cancel'),
+  clearSubmitForm: document.querySelector(".form-group__btn-cancel"),
 
   // Submit form
-
-  inputWinRate: document.getElementById('article_win_rate'),
-  inputCTR: document.getElementById('article_ctr'),
+  inputWinRate: document.getElementById("article_win_rate"),
+  inputCTR: document.getElementById("article_ctr")
 };
 
 //Get All Articles
@@ -63,35 +48,112 @@ function handleGetArticles() {
     })
     .then(data => {
       let arrObjArticle = data.articles;
-      let sortAsc = document.querySelectorAll(".sort__asc");
-      let hello = document.querySelector(".hello");
-
-      for (let i = 0; i < sortAsc.length; i++) {
-        sortAsc[i].addEventListener("click", x => {
-          console.log(x.target.value);
-        });
-      }
-      let displayArticle = "";
-      arrObjArticle.map((objArticle, i) => {
-
-        displayArticle = displayArticle.concat(`
-        <tr class="tbody-row">
-        <td ><a title="Go to Article ${objArticle.article_title}" href=#" class="link-article move-right">${objArticle.article_title}</a></td>
-        <td >${objArticle.article_published}</td>
-        <td >${objArticle.article_site}</td>
-        <td ><a title="Go to Ad group ${objArticle.article_ad_group}" href="" class="link-article move-right">${objArticle.article_ad_group}</a></td>
-        <td >${objArticle.article_bids}</td>
-        <td >${objArticle.article_spending}</td>
-        <td >${objArticle.article_win_rate} %</td>
-        <td >${objArticle.article_impressions}</td>
-        <td >${objArticle.article_clicks}</td>
-        <td >${objArticle.article_ctr} %</td>
-        </tr>
-        `);
-      });
-      domControl.tableArticleBody.innerHTML = displayArticle;
+      // Pass arrObjArticle to HandleArticlesOutput class
+      new HandleArticlesOutput(arrObjArticle);
     })
     .catch(err => console.log(err));
+}
+
+class HandleArticlesOutput {
+  constructor(articles) {
+    this.articles = articles;
+    // For pagination
+    this.totalArticlesLength = this.articles.length;
+    this.showDefaultLength = 6;
+    this.showIncrementLengthBy = 4;
+
+    this.displayArticle();
+    this.controlPagination();
+    this.sortArticle();
+  }
+  controlPagination() {
+    let currentShow = 0;
+    this.show = 0;
+    // Show number of total articles
+    DOMcontrol.totalArticleNumber.innerHTML = this.totalArticlesLength;
+    // Show number of default loaded article number 
+    DOMcontrol.totalLoadedArticleNumber.innerHTML = this.showDefaultLength;
+    DOMcontrol.btnLoadMore.addEventListener("click", e => {
+      e.preventDefault();
+      this.showMore = currentShow + this.showIncrementLengthBy;
+      currentShow = currentShow + this.showIncrementLengthBy;
+      //Control increment length if increment length greater then total length of articles 
+      this.show = (this.showMore + this.showDefaultLength > this.articles.length) ? this.articles.length - this.showDefaultLength : this.showMore;
+
+      // Show number of loaded articles
+      DOMcontrol.totalLoadedArticleNumber.innerHTML = this.showDefaultLength + this.show;
+      //Update the :: displayArticle() :: each time user click on 'Load more' button  
+      this.displayArticle();
+    });
+  }; // EOF controlPagination
+
+  displayArticle() {
+    // Condition: before user click on 'Load more' button and after click it
+    let showMore = (this.show) ? this.show : 0;
+    let showArticleLength = this.showDefaultLength + showMore;
+    let displayArticle = "";
+    for (let i = 0; i < showArticleLength; i++) {
+      displayArticle = displayArticle.concat(`
+        <tr class="tbody-row">
+        <td ><a title="Go to Article ${this.articles[i].article_title}" href=#" class="link-article move-right">${this.articles[i].article_title}</a></td>
+        <td >${this.articles[i].article_published}</td>
+        <td >${this.articles[i].article_site}</td>
+        <td ><a title="Go to Ad group ${this.articles[i].article_ad_group}" href="" class="link-article move-right">${this.articles[i].article_ad_group}</a></td>
+        <td class="text-center" >${this.articles[i].article_bids}</td>
+        <td class="text-center" >${this.articles[i].article_spending}</td>
+        <td class="text-center" >${this.articles[i].article_win_rate} %</td>
+        <td class="text-center" >${this.articles[i].article_impressions}</td>
+        <td class="text-center" >${this.articles[i].article_clicks}</td>
+        <td class="text-center">${this.articles[i].article_ctr} %</td>
+        </tr>
+        `);
+    }
+    // :::::: RENDER DATA USING MAP METHOD :::: 
+    // this.articles.map((objArticle, i) => {
+    //   displayArticle = displayArticle.concat(`
+    //     <tr class="tbody-row">
+    //     <td ><a title="Go to Article ${objArticle.article_title}" href=#" class="link-article move-right">${objArticle.article_title}</a></td>
+    //     <td >${objArticle.article_published}</td>
+    //     <td >${objArticle.article_site}</td>
+    //     <td ><a title="Go to Ad group ${objArticle.article_ad_group}" href="" class="link-article move-right">${objArticle.article_ad_group}</a></td>
+    //     <td class="text-center" >${objArticle.article_bids}</td>
+    //     <td class="text-center" >${objArticle.article_spending}</td>
+    //     <td class="text-center" >${objArticle.article_win_rate} %</td>
+    //     <td class="text-center" >${objArticle.article_impressions}</td>
+    //     <td class="text-center" >${objArticle.article_clicks}</td>
+    //     <td class="text-center">${objArticle.article_ctr} %</td>
+    //     </tr>
+    //     `);
+    // });
+    DOMcontrol.tableArticleBody.innerHTML = displayArticle;
+  }; // EOF displayArticles
+
+  sortArticle() {
+    let sortAscItems = document.querySelectorAll(".sort__asc");
+    for (let i = 0; i < sortAscItems.length; i++) {
+      sortAscItems[i].addEventListener("click", e => {
+        let sortBy = e.target.getAttribute("name");
+        console.log(sortBy);
+        let array = sortArt(this.articles, sortBy);
+        this.displayArticle();
+      });
+    }
+
+    function sortArt(array, sortBy) {
+      array.sort(function (a, b) {
+        let x = a[sortBy];
+        ///a[sortBy] > b[sortBy] ? 1 : -1;
+        if (a[sortBy] > b[sortBy]) {
+          return 1;
+        } else {
+          return -1;
+        }
+      });
+      return array;
+    }
+
+  }
+
 }
 
 //Create Article
@@ -103,16 +165,16 @@ function handleCreateArticle(formData) {
     })
     .then(res => res.json())
     .then(data => {
-      // Display success or failure message 
+      // Display success or failure message
 
-      domControl.submitMessage.innerHTML = `<span class="submit-message">${data.message}</span>`;
+      DOMcontrol.submitMessage.innerHTML = `<span class="submit-message">${data.message}</span>`;
       // Remove message after two second
       setTimeout(() => {
-        domControl.submitMessage.innerHTML = "";
+        DOMcontrol.submitMessage.innerHTML = "";
       }, 2000);
 
       //Empty the form after submit
-      domControl.submitArticleForm.reset();
+      DOMcontrol.submitArticleForm.reset();
 
       // Call handleGetArticles function
       handleGetArticles();
@@ -140,33 +202,33 @@ class HandleStylePopupOverlay {
     this.stylesheet = {
       show: {
         top: 0,
-        display: 'block'
+        display: "block"
       },
       hide: {
-        top: '-150%'
+        top: "-150%"
       }
     };
-    this.applyStylesheet()
+    this.applyStylesheet();
   }
 
   applyStylesheet() {
     // Show popUp overlay
-    domControl.linkCreateArticle.addEventListener('click', (e) => {
+    DOMcontrol.linkCreateArticle.addEventListener("click", e => {
       e.preventDefault();
-      let addStyle = domControl.popupOverlay.style;
+      let addStyle = DOMcontrol.popupOverlay.style;
       for (let style in this.stylesheet.show) {
         addStyle[style] = this.stylesheet.show[style];
       }
     });
 
     // Hide and Display non popUp overlay
-    domControl.closePopupOverlay.addEventListener('click', (e) => {
+    DOMcontrol.closePopupOverlay.addEventListener("click", e => {
       e.preventDefault();
-      let addStyle = domControl.popupOverlay.style;
+      let addStyle = DOMcontrol.popupOverlay.style;
       for (let style in this.stylesheet.hide) {
         addStyle[style] = this.stylesheet.hide[style];
       }
-    })
+    });
   }
 }
 new HandleStylePopupOverlay();
@@ -175,18 +237,18 @@ new HandleStylePopupOverlay();
 //****************** FORM VALIDATION *************/
 // Validate submit form:: Input value in percentage
 function validateInputValueInPercentage(e) {
-  // if input value is > 100 (Note: percent should be less of equal to 100)
+  // if input value is > 100 (Note: percent should be less or equal to 100)
   if (e.value > 100) {
     e.value = e.value.slice(0, 2);
   }
   // if the input value in negative (Note: percentage should be +ve numer)
   if (e.value < 0) {
-    e.value = e.value.slice(1)
+    e.value = e.value.slice(1);
   }
 }
 
 // Clear submit form
-domControl.clearSubmitForm.addEventListener('click', (e) => {
+DOMcontrol.clearSubmitForm.addEventListener("click", e => {
   e.preventDefault();
-  domControl.submitArticleForm.reset();
-})
+  DOMcontrol.submitArticleForm.reset();
+});
